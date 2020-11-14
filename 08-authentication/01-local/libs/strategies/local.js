bffd706e960bcb66064a015b0e4d4fac0894b547
belozerovany <n.belozyorova@gmail.com>
2020-11-14 23:29:39 +0300

    Fix for correct error handling

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -3,23 +3,25 @@ const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      User.findOne({email: email }, async function (err, user) {
-          if (err) return done(err);
+    async function(email, password, done) {
+      try {
+        user = await User.findOne({email: email });
 
-          if (!user) {
-            done(null, false, 'Нет такого пользователя');
-            return;
-          }
+        if (!user) {
+          done(null, false, 'Нет такого пользователя');
+          return;
+        }
 
-          passwordMatch = await user.checkPassword(password);
+        passwordMatch = await user.checkPassword(password);
 
-          if (!passwordMatch) {
-            done(null, false, 'Неверный пароль');
-            return;
-          }
+        if (!passwordMatch) {
+          done(null, false, 'Неверный пароль');
+          return;
+        }
 
-          done(null, user);
-      });
+        done(null, user);
+      } catch(err) {
+        done(err);
+      }
     }
 );
